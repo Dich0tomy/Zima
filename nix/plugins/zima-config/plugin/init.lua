25d972dd91adc2792a6f916982b92a74dfff79dd
b4mbus <github@yonderly.addy.io>
2024-06-22 03:29:08 +0200

    feat: load config as the last plugin

--- nix/plugins/zima-config/plugin/init.lua
+++ nix/plugins/zima-config/plugin/init.lua
@@ -1,1 +1,9 @@
-require('zima-config')
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = "Loads zima config only after all other setup (including other plugins) has completed.",
+  group = vim.api.nvim_create_namespace('zima-startup'),
+
+  once = true,
+  callback = function()
+    require('zima-config')
+  end
+})
