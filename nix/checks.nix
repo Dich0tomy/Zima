927c79ee59ea557dfe25ad2428e4de8a206a83a5
b4mbus <github@yonderly.addy.io>
2024-06-22 02:14:12 +0200

    feat(nix): evaluate config as nix check

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,15 +1,24 @@
-{pkgs, ...}: {
+# FIXME: for some reason this doesn't work, I get no output in terminal lmao
+{
+  pkgs,
+  self',
+  ...
+}: {
   checks.default = pkgs.writeShellApplication {
     name = "zima-checks";
 
     runtimeInputs = [
       pkgs.gum
       pkgs.typos
+      self'.packages.default
     ];
 
     text = ''
       gum style --border="rounded" --padding="0 1" --bold --border-foreground="#ffc0cb" "Running typos!"
       typos .
+
+      gum style --border="rounded" --padding="0 1" --bold --border-foreground="#ffc0cb" "Checking zima!"
+      zima --headless +exit
     '';
   };
 }
