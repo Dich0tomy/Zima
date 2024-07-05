ff0ad816a99afb6bdd286ddafe65844c5ca4c264
b4mbus <github@yonderly.addy.io>
2024-07-05 23:36:38 +0200

    feat: add a git module

---
+++ nix/plugins/zima-config/lua/zima-config/modules/git.lua
@@ -0,0 +1,176 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    event = 'BufRead',
+    after = function()
+      require('gitsigns').setup {
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged_enable = true,
+        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir = {
+          follow_files = true
+        },
+        auto_attach = true,
+        attach_to_untracked = false,
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        preview_config = {
+          -- Options passed to nvim_open_win
+          border = 'single',
+          style = 'minimal',
+          relative = 'cursor',
+          row = 0,
+          col = 1
+        },
+      }
+    end
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = "nvim-lua/plenary.nvim",
+    cmd = {
+      'NG',
+      'Neogit'
+    },
+    after = function()
+      require('neogit').setup({
+        disable_signs = false,
+        disable_hint = true,
+        disable_context_highlighting = false,
+        disable_commit_confirmation = false,
+        -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+        -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+        auto_refresh = true,
+        -- Value used for `--sort` option for `git branch` command
+        -- By default, branches will be sorted by commit date descending
+        -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
+        -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
+        sort_branches = "-committerdate",
+        disable_builtin_notifications = false,
+        -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'
+        telescope_sorter = nil,
+        use_magit_keybindings = false,
+        -- Change the default way of opening neogit
+        kind = "tab",
+        -- The time after which an output console is shown for slow running commands
+        console_timeout = 5000,
+        -- Automatically show console if a command takes more than console_timeout milliseconds
+        auto_show_console = true,
+        -- Persist the values of switches/options within and across sessions
+        remember_settings = true,
+        -- Scope persisted settings on a per-project basis
+        use_per_project_settings = true,
+        -- Array-like table of settings to never persist. Uses format "Filetype--cli-value"
+        --   ie: `{ "NeogitCommitPopup--author", "NeogitCommitPopup--no-verify" }`
+        ignored_settings = {},
+        -- Change the default way of opening the commit popup
+        commit_popup = {
+          kind = "split",
+        },
+        -- Change the default way of opening the preview buffer
+        preview_buffer = {
+          kind = "split",
+        },
+        -- Change the default way of opening popups
+        popup = {
+          kind = "split",
+        },
+        -- customize displayed signs
+        signs = {
+          -- { CLOSED, OPENED }
+          section = { ">", "v" },
+          item = { ">", "v" },
+          hunk = { "", "" },
+        },
+        -- Integrations are auto-detected, and enabled if available, but can be disabled by setting to "false"
+        integrations = {
+          -- If enabled, use telescope for menu selection rather than vim.ui.select.
+          -- Allows multi-select and some things that vim.ui.select doesn't.
+          telescope = true,
+
+          -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
+          -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+          --
+          -- Requires you to have `sindrets/diffview.nvim` installed.
+          -- use {
+          --   'NeogitOrg/neogit',
+          --   requires = {
+          --     'nvim-lua/plenary.nvim',
+          --     'sindrets/diffview.nvim'
+          --   }
+          -- }
+          --
+          diffview = true,
+        },
+        -- Setting any section to `false` will make the section not render at all
+        sections = {
+          untracked = {
+            folded = false
+          },
+          unstaged = {
+            folded = false
+          },
+          staged = {
+            folded = false
+          },
+          stashes = {
+            folded = true
+          },
+          unpulled = {
+            folded = true
+          },
+          unmerged = {
+            folded = false
+          },
+          recent = {
+            folded = true
+          },
+        },
+        -- override/add mappings
+        mappings = {
+          -- modify status buffer mappings
+          status = {
+            -- Adds a mapping with "B" as key that does the "BranchPopup" command
+            ["B"] = "BranchPopup",
+            -- Removes the default mapping of "s"
+            ["s"] = "",
+          },
+          -- Modify fuzzy-finder buffer mappings
+          finder = {
+            -- Binds <cr> to trigger select action
+            ["<cr>"] = "select",
+          }
+        }
+      })
+    end
+  },
+}
