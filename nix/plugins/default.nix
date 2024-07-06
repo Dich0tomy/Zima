a46fe1c58ba6611d28908c9c63d79865a7141fea
b4mbus <github@yonderly.addy.io>
2024-07-06 19:56:00 +0200

    ref(nix): remove the kaizen-framework

--- nix/plugins/default.nix
+++ nix/plugins/default.nix
@@ -1,7 +1,5 @@
 {
   self,
-  nixpkgs,
-  system,
   ...
 }: {
   perSystem = {
@@ -10,11 +8,9 @@
     npins',
     ...
   }: {
-    # https://github.com/otavioschwanck/arrow.nvim
     legacyPackages.vimPlugins =
       {
         zima-config = pkgs.callPackage ./zima-config {inherit self;};
-        kaizen-framework = pkgs.callPackage ./kaizen-framework {inherit self;};
       }
       // (import ./npins-plugins (pkgs // {inherit npins';}));
   };
