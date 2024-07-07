968339500d93454b17c3fb17a1f7f19b65a24aaa
b4mbus <github@yonderly.addy.io>
2024-07-07 16:48:32 +0200

    ref(lib.modules): split module loading into functions

--- nix/plugins/zima-config/lua/zima-config/lib/modules.lua
+++ nix/plugins/zima-config/lua/zima-config/lib/modules.lua
@@ -1,14 +1,13 @@
+local fp = require('zima-config.lib.fp')
+local iter = require('zima-config.lib.iter')
+
 local M = {}
 
----Loads a list of modules and extracts plugin info from them
----@param mods any func to wrap
----@param provider any which provider to use, default = lz.n
-function M.load_modules(mods, provider)
-  provider = provider or 'lz.n'
-
-  local fp = require('zima-config.lib.fp')
-  local iter = require('zima-config.lib.iter')
-
+---Partitions modules to requireable ones and ones not found
+---@param mods string[] Modules to load
+---@return string[] good # Good modules
+---@return string[] bad # Bad modules
+local function partition_modules(mods)
   local specs = vim.iter(mods)
     :map(function(modname)
       return { modname, vim.tbl_isempty(vim.loader.find(modname)) }
@@ -19,29 +18,36 @@ function M.load_modules(mods, provider)
 
   local good, bad = iter.partition(specs, fp.chain(unpack, fp.second, fp.equal(false)))
 
+  return
+    vim.iter(good):map(unwrap_mods):totable(),
+    vim.iter(bad):map(unwrap_mods):totable()
+end
+
+---Notifies the user about not found modules
+---@param bad string[] Module paths
+local function notify_bad(bad)
   if #bad ~= 0 then
     local error = vim.iter(bad)
-      :map(unwrap_mods)
       :map(fp.chain(vim.inspect, fp.fmt('- %s')))
       :join('\n')
 
     zima.error('Failed to load the following modules:\n%s', error)
   end
+end
 
-  local provider_ok, possible_provided = zima.pequire(provider)
-  if not provider_ok then
-    zima.error('Cannot require the provider!\n%s', possible_provided)
-    return
-  end
+---Loads a list of modules and extracts plugin info from them
+---@param mods string[] Paths to modules
+function M.load_modules(mods)
+  local good, bad = partition_modules(mods)
+
+  notify_bad(bad)
 
   local loaded_good = vim.iter(good)
-    :map(fp.chain(unwrap_mods, require))
+    :map(require)
     :flatten()
     :totable()
 
-  zima.trace('Loaded goods: %s', loaded_good)
-
-  local setup_ok, possible_error = pcall(possible_provided.load, loaded_good)
+  local setup_ok, possible_error = pcall(require('lz.n').load, loaded_good)
   if not setup_ok then
     zima.error('While trying to setup the provider with specs!\n%s', possible_error)
   end
